c62511493fe436034d8374cffcc8abc1a6cf761c
Jae-Won Chung <jwnchung@umich.edu>
2024-02-22 13:07:58 -0500

    Increase width to 400

--- src/cards/repo-card.js
+++ src/cards/repo-card.js
@@ -152,7 +152,7 @@ const renderRepoCard = (repo, options = {}) => {
   const card = new Card({
     defaultTitle: header.length > 35 ? `${header.slice(0, 35)}...` : header,
     titlePrefixIcon: icons.contribs,
-    width: 350,
+    width: 400,
     height,
     border_radius,
     colors,
